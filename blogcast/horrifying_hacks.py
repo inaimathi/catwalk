7f53735b4cc0ec5216c7587f485657ab0f990304
inaimathi <leo.zovic@gmail.com>
2023-10-23 14:04:14 -0400

    Factor out acronyms in horrifying_hacks

--- blogcast/horrifying_hacks.py
+++ blogcast/horrifying_hacks.py
@@ -1,31 +1,63 @@
 import re
 
+LETTER_PRONOUNCIATIONS = {
+    "a": "eh",
+    "b": "bee",
+    "c": "see",
+    "d": "dee",
+    "f": "eff",
+    "g": "jee",
+    "h": "eh ch",
+    "i": "eye",
+    "j": "jay",
+    "k": "kay",
+    "l": "el",
+    "m": "em",
+    "n": "en",
+    "o": "oh",
+    "p": "pee",
+    "q": "cue",
+    "r": "are",
+    "s": "ess",
+    "t": "tee",
+    "u": "you",
+    "v": "vee",
+    "w": "double you",
+    "x": "ecks",
+    "y": "why",
+    "z": "zee"
+}
+
+def _acronym(acronym):
+    letters = [LETTER_PRONOUNCIATIONS[lt] for lt in acronym.lower()]
+    return " ".join(letters)
+
 MISPRONOUNCED_TOKENS = {
     "chatgpt": "Chat jee pee tee",
-    "tts": "tee tee ess",
-    "openai": "open ai eye",
-    "api": "eh pee eye",
-    "ai": "eh eye",
-    "gpt-4": "jee pee tee four",
-    "gpt-3": "jee pee tee three",
+    "openai": "open eh eye",
     "strachan": "strohn",
-    "ssh": "ess ess eh ch",
-    "http": "eh ch tee tee pee",
-    "url": "you are el"
 }
+
+ACRONYMS = { "gpt", "ai", "api", "tts", "ssh", "http", "url","amd", "cpu", "tldr", "lts" }
 
 UNICODE = {
     "🤗": "hugging face"
 }
 
 RE = re.compile(r'(?:\b(?:%s|{u"\U0001F600-\U0001F64F"})\b)|(?:%s)' % (
-    '|'.join(MISPRONOUNCED_TOKENS.keys()),
+    '|'.join(ACRONYMS.union(MISPRONOUNCED_TOKENS.keys())),
     '|'.join(UNICODE.keys())
 ), flags=re.IGNORECASE | re.UNICODE)
 
 def _replace(m):
     low = m.group(0).lower()
-    return MISPRONOUNCED_TOKENS.get(low, UNICODE.get(low, m.group(0)))
+    if low in ACRONYMS:
+        return _acronym(low)
+    if low in MISPRONOUNCED_TOKENS:
+        return MISPRONOUNCED_TOKENS[low]
+    if low in UNICODE:
+        return UNICODE[low]
+    return m.group(0)
 
 def apply(string):
     return re.subn(RE, _replace, string)[0]
