2d61c8251a2b17c38b06bf99c25189099c68bdf5
inaimathi <leo.zovic@gmail.com>
2023-10-26 23:44:24 -0400

    Try the trivial GPU resource allocation approach

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 
 import tqdm
@@ -13,12 +14,14 @@ app = Flask(__name__)
 if not os.path.exists("static"):
     os.makedirs("static")
 
+GPU = asyncio.Lock()
+
 @app.route("/health")
 def health():
     return jsonify({"status": "ok"})
 
 @app.post("/v0/audio/transcribe")
-def run_transcribe():
+async def run_transcribe():
     if 'file' not in request.files:
         return jsonify({"status": "error", "message": "request must have a file"}), 400
     file = request.files['file']
@@ -34,7 +37,8 @@ def run_transcribe():
     path = util.fresh_file("tmp-transcription-audio-", os.path.splitext(file.filename))
     file.save(path)
 
-    return jsonify({"status": "ok", "result": basics.transcribe(path)})
+    async with GPU:
+        return jsonify({"status": "ok", "result": basics.transcribe(path)})
 
 @app.get("/v0/audio/tts")
 def tts_properties():
@@ -43,7 +47,7 @@ def tts_properties():
     })
 
 @app.post("/v0/audio/tts")
-def tts_run():
+async def tts_run():
     text = request.values.get('text')
     if text is None:
         return jsonify({"status": "error", "message": "request must have text"}), 400
@@ -51,7 +55,8 @@ def tts_run():
     voice = request.values.get("voice", "leo")
     k = int(request.values.get("k", "1"))
 
-    res = tts.text_to_wavs(text, voice=voice, k=k)
+    async with GPU:
+        res = tts.text_to_wavs(text, voice=voice, k=k)
 
     return jsonify({
         "status": "ok", "voice": voice, "text": text,
@@ -59,7 +64,7 @@ def tts_run():
     })
 
 @app.post("/v0/audio/blogcast")
-def read_blog_post():
+async def read_blog_post():
     url = request.values.get('url')
     if url is None:
         return jsonify({"status": "error", "message": "request must have a target URL"}), 400
@@ -68,14 +73,15 @@ def read_blog_post():
 
     app.logger.debug(f"blogcast -- reading '{url}' as '{voice}'...")
 
-    script = blogcast.script.script_from(url)
-    res = [
-        {
-            "text": el,
-            "url": f"/static/{os.path.basename(tts.text_to_wavs(el, voice=voice, k=1)[0])}"
-        } if isinstance(el, str) else el
-        for el in tqdm.tqdm(script)
-    ]
+    async with GPU:
+        script = blogcast.script.script_from(url)
+        res = [
+            {
+                "text": el,
+                "url": f"/static/{os.path.basename(tts.text_to_wavs(el, voice=voice, k=1)[0])}"
+            } if isinstance(el, str) else el
+            for el in tqdm.tqdm(script)
+        ]
 
     return jsonify({
         "status": "ok", "voice": voice, "target": url,
@@ -93,7 +99,8 @@ def run_generate_text():
         return jsonify({"status": "error", "message": "request must have prompt"}), 400
     max_new_tokens = request.values.get("max_new_tokens")
 
-    return jsonify({"status": "ok", "result": basics.generate_text(prompt, max_new_tokens)})
+    async with GPU:
+        return jsonify({"status": "ok", "result": basics.generate_text(prompt, max_new_tokens)})
 
 @app.post("/v0/image/describe")
 def run_describe():
