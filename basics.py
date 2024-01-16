8fd7b00129ddb9a7ef3e99c97a24c42c62b86ed1
inaimathi <leo.zovic@gmail.com>
2024-01-16 01:09:55 -0500

    Part 1 of major job refactor

--- basics.py
+++ basics.py
@@ -9,32 +9,37 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 import util
 
 
-def transcribe(audio_file, gpu="1080"):
-    _WHISPER = whisper.load_model("base")
-    util.to_gpu(_WHISPER, gpu)
+def transcribe(audio_file):
+    model = whisper.load_model("base")
+    model.to("cuda")
     audio = whisper.load_audio(audio_file)
     audio = whisper.pad_or_trim(audio)
-    mel = whisper.log_mel_spectrogram(audio).to(_WHISPER.device)
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    _, probs = _WHISPER.detect_language(mel)
+    _, probs = model.detect_language(mel)
     opts = whisper.DecodingOptions()
 
-    result = whisper.decode(_WHISPER, mel, opts)
+    result = whisper.decode(model, mel, opts)
 
     return result.text
 
+
 _CAPTION = None
+
 
 def caption_image(url):
     global _CAPTION
     if _CAPTION is None:
         print("Loading CAPTION model...")
-        _CAPTION = pipeline("image-to-text", model="Salesforce/blip2-flan-t5-xl") # "Salesforce/blip-image-captioning-base"
+        _CAPTION = pipeline(
+            "image-to-text", model="Salesforce/blip2-flan-t5-xl"
+        )  # "Salesforce/blip-image-captioning-base"
     resp = requests.get(url, headers=util.FF_HEADERS, stream=True)
     if resp.status_code == 200:
         img = PIL.Image.open(resp.raw)
-        return _CAPTION(img)[0]['generated_text']
+        return _CAPTION(img)[0]["generated_text"]
     return ""
+
 
 # print("Loading INSTRUCT...")
 # _TEXT_MODEL = "tiiuae/falcon-7b-instruct"
@@ -50,23 +55,32 @@ def caption_image(url):
 
 def generate_text(prompt, max_new_tokens=50):
     return None
-    return _INSTRUCT(
-        prompt, do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=_TOKENIZER.eos_token_id,
-        max_new_tokens=max_new_tokens
-    )
+    # return _INSTRUCT(
+    #     prompt,
+    #     do_sample=True,
+    #     top_k=10,
+    #     num_return_sequences=1,
+    #     eos_token_id=_TOKENIZER.eos_token_id,
+    #     max_new_tokens=max_new_tokens,
+    # )
+
 
 def summarize_code(code_block):
     res = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system",
-             "content": "You are an omni-competent programmer and brilliant documentation writer. You're usually the stickler that insists on better docstrings being written when you're on a team. You know all programming languages, and have an impeccable skill for explaining code written in them to others. You will be asked to summarize a code block. Assume that this is a description that will have to be read out loud to someone familiar with the language it is written in, but not what the specific code itself does. You should tell the listener what language the code is written in, and what it does at a high level. Keep it short; on the order of two to five sentences."},
-            {"role": "user",
-             "content": f"Please summarize the following code block: {code_block}"}])
+            {
+                "role": "system",
+                "content": "You are an omni-competent programmer and brilliant documentation writer. You're usually the stickler that insists on better docstrings being written when you're on a team. You know all programming languages, and have an impeccable skill for explaining code written in them to others. You will be asked to summarize a code block. Assume that this is a description that will have to be read out loud to someone familiar with the language it is written in, but not what the specific code itself does. You should tell the listener what language the code is written in, and what it does at a high level. Keep it short; on the order of two to five sentences.",
+            },
+            {
+                "role": "user",
+                "content": f"Please summarize the following code block: {code_block}",
+            },
+        ],
+    )
     return res.choices[0].message.content
+
 
 # def summarize_code(code_block, max_new_tokens=200):
 #     res = _INSTRUCT(
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import asyncio
 import json
 import os
-import re
-import subprocess
-import threading
 
 import tornado
 import tqdm
@@ -11,6 +8,7 @@ import tqdm
 import basics
 import blogcast.script
 import images
+import model
 import tts
 import util
 
@@ -18,45 +16,51 @@ if not os.path.exists("static"):
     os.makedirs("static")
 
 GPU = asyncio.Semaphore(1)
-STATIC_PORT = 8081
 
-if os.path.exists("))blacklist.txt"):
-    with open("blacklist.txt", 'r') as f:
+if os.path.exists("blacklist.txt"):
+    with open("blacklist.txt", "r") as f:
         IP_BLACKLIST = set(f.read().splitlines())
 else:
     IP_BLACKLIST = set([])
 
+
 def _BAN(ip):
     print(f"BANNING {ip}...")
-    with open("blacklist.txt", 'a+') as bl:
+    with open("blacklist.txt", "a+") as bl:
         bl.write(f"{ip}\n")
     IP_BLACKLIST.add(ip)
 
+
 class JSONHandler(tornado.web.RequestHandler):
-    SUPPORTED_METHODS = tornado.web.RequestHandler.SUPPORTED_METHODS + ('CONNECT',)
+    SUPPORTED_METHODS = tornado.web.RequestHandler.SUPPORTED_METHODS + ("CONNECT",)
+
     def prepare(self):
         if self.request.remote_ip in IP_BLACKLIST:
-            self.json({"status": "whoops"}, 403)
+            return self.json({"status": "whoops"}, 403)
 
     def set_default_headers(self):
         self.set_header("Content-Type", "application/json")
 
     def connect(self):
         _BAN(self.request.remote_ip)
-        self.json({"status": "looks like you're going to the shadow realm, Jimbo"}, 400)
+        return self.json(
+            {"status": "looks like you're going to the shadow realm, Jimbo"}, 400
+        )
 
     def json(self, data, status=None):
         if status is not None:
             self.set_status(status)
-        self.write(json.dumps(data))
+        return self.write(json.dumps(data))
+
 
 class TrapCard(JSONHandler):
     def prepare(self):
         if self.request.remote_ip in IP_BLACKLIST:
             return self.json({"status": "whoops"}, 403)
         _BAN(self.request.remote_ip)
-        self.json({"status": "looks like you're going to the shadow realm, Jimbo"}, 400)
-        return
+        return self.json(
+            {"status": "looks like you're going to the shadow realm, Jimbo"}, 400
+        )
 
 
 class HealthHandler(JSONHandler):
@@ -65,39 +69,55 @@ class HealthHandler(JSONHandler):
         res = {"status": "ok"}
         if val is not None:
             res["value"] = val
-        self.json(res)
+        return self.json(res)
+
 
 class TranscribeHandler(JSONHandler):
     async def post(self):
-        if 'file' not in request.files:
-            return self.json({"status": "error", "message": "request must have a file"}, 400)
-        file = self.request.files['file'][0]
-        if file['filename'] == '':
-            return self.json({"status": "error", "message": "request must have a file"}, 400)
+        if "file" not in self.request.files:
+            return self.json(
+                {"status": "error", "message": "request must have a file"}, 400
+            )
+        file = self.request.files["file"][0]
+        if file["filename"] == "":
+            return self.json(
+                {"status": "error", "message": "request must have a file"}, 400
+            )
         allowed_extensions = {".wav", ".mp3"}
-        if not os.path.splitext(file['filename'])[1].lower() in allowed_extensions:
-            return self.json({
-                "status": "error",
-                "message": f"file must be one of {', '.join(allowed_extensions)}"
-            }, 400)
+        if not os.path.splitext(file["filename"])[1].lower() in allowed_extensions:
+            return self.json(
+                {
+                    "status": "error",
+                    "message": f"file must be one of {', '.join(allowed_extensions)}",
+                },
+                400,
+            )
 
-        with open(util.fresh_file("tmp-transcription-audio-", os.path.splitext(file.filename)), 'wb') as out:
-            out.write(file['body'])
+        with open(
+            util.fresh_file(
+                "tmp-transcription-audio-", os.path.splitext(file.filename)
+            ),
+            "wb",
+        ) as out:
+            out.write(file["body"])
 
         async with GPU:
-            return jsonify({"status": "ok", "result": basics.transcribe(path)})
+            return self.json({"status": "ok", "result": basics.transcribe(path)})
+
 
 class ImageHandler(JSONHandler):
     async def post(self):
         prompt = self.get_argument("prompt")
         if prompt is None:
-            return self.json({"status": "error", "message": "request must have prompt"}, 400)
+            return self.json(
+                {"status": "error", "message": "request must have prompt"}, 400
+            )
 
         negative_prompt = self.get_argument("negative_prompt", None)
         k = int(self.get_argument("k", "1"))
-        width=int(self.get_argument("width", "1024"))
-        height=int(self.get_argument("height", "1024"))
-        steps=int(self.get_argument("steps", "50"))
+        width = int(self.get_argument("width", "1024"))
+        height = int(self.get_argument("height", "1024"))
+        steps = int(self.get_argument("steps", "50"))
         seed = self.get_argument("seed", None)
         if seed is not None:
             seed = int(seed)
@@ -105,25 +125,35 @@ class ImageHandler(JSONHandler):
         res = []
         for _ in range(k):
             async with GPU:
-                path = images.generate_image(prompt, negative_prompt, width=width, height=height, steps=steps, seed=seed)
+                path = images.generate_image(
+                    prompt,
+                    negative_prompt,
+                    width=width,
+                    height=height,
+                    steps=steps,
+                    seed=seed,
+                )
                 res.append(path)
 
-        self.json({
-            "status": "ok", "prompt": prompt,
-            "urls": [f"/{os.path.basename(path)}" for path in res],
-            "port": STATIC_PORT
-        })
+        return self.json(
+            {
+                "status": "ok",
+                "prompt": prompt,
+                "urls": [f"/{os.path.basename(path)}" for path in res],
+            }
+        )
+
 
 class TTSHandler(JSONHandler):
     def get(self):
-        self.json({
-            "voices": tts.get_voices()
-        })
+        return self.json({"voices": tts.get_voices()})
 
     async def post(self):
-        text = self.get_argument('text')
+        text = self.get_argument("text")
         if text is None:
-            return self.json({"status": "error", "message": "request must have text"}, 400)
+            return self.json(
+                {"status": "error", "message": "request must have text"}, 400
+            )
 
         voice = self.get_argument("voice", "leo")
         k = int(self.get_argument("k", "1"))
@@ -131,18 +161,23 @@ class TTSHandler(JSONHandler):
         async with GPU:
             res = tts.text_to_wavs(text, voice, k)
 
-        self.json({
-            "status": "ok", "voice": voice, "text": text,
-            "urls": [f"/{os.path.basename(r)}" for r in res],
-            "port": STATIC_PORT
-        })
+        return self.json(
+            {
+                "status": "ok",
+                "voice": voice,
+                "text": text,
+                "urls": [f"/{os.path.basename(r)}" for r in res],
+            }
+        )
 
 
 class BlogcastHandler(JSONHandler):
     async def post(self):
-        url = self.get_argument('url')
+        url = self.get_argument("url")
         if url is None:
-            return self.json({"status": "error", "message": "request must have a target URL"}, 400)
+            return self.json(
+                {"status": "error", "message": "request must have a target URL"}, 400
+            )
 
         voice = self.get_argument("voice", "leo")
 
@@ -156,52 +191,73 @@ class BlogcastHandler(JSONHandler):
             if isinstance(el, str):
                 async with GPU:
                     res_tts = tts.text_to_wavs(el, voice, 1)
-                res.append({
-                    "text": el,
-                    "url": f"/{os.path.basename(res_tts[0])}",
-                    "port": STATIC_PORT
-                })
+                res.append({"text": el, "url": f"/{os.path.basename(res_tts[0])}"})
             else:
                 res.append(el)
 
-        print(f"   done speaking...")
-        response = {
-            "status": "ok", "voice": voice, "target": url,
-            "result": res,
-            "port": STATIC_PORT
-        }
-        with open(util.fresh_file("blogcast-result-", ".json"), 'w') as out:
+        print("   done speaking...")
+        response = {"status": "ok", "voice": voice, "target": url, "result": res}
+        with open(util.fresh_file("blogcast-result-", ".json"), "w") as out:
             out.write(json.dumps(response))
-        self.json(response)
+        return self.json(response)
 
 
 class ChatHandler(JSONHandler):
     def post(self):
         return self.json({"status": "ok", "stub": "TODO"})
 
+
 class TextCompletionHandler(JSONHandler):
     async def post(self):
         prompt = self.get_argument("prompt")
         if prompt is None:
-            return self.json({"status": "error", "message": "request must have prompt"}, 400)
+            return self.json(
+                {"status": "error", "message": "request must have prompt"}, 400
+            )
 
         max_new_tokens = self.get_argument("max_new_tokens")
 
         async with GPU:
-            return self.json({"status": "ok", "result": basics.generate_text(prompt, max_new_tokens)})
+            return self.json(
+                {"status": "ok", "result": basics.generate_text(prompt, max_new_tokens)}
+            )
+
 
 class DescribeImageHandler(JSONHandler):
     async def post(self):
         url = self.get_argument("url")
         if url is None:
-            self.json({"status": "error", "message": "request must have url"}, 400)
+            return self.json(
+                {"status": "error", "message": "request must have url"}, 400
+            )
 
         async with GPU:
             return self.json({"status": "ok", "result": basics.caption_image(url)})
 
+
 class UIHandler:
     def get(self):
         self.render("static/index.html")
+
+
+class JobsHandler(JSONHandler):
+    def options(self):
+        return self.json({"TODO": "return info on differnet job types you can start"})
+
+    def get(self):
+        return self.json({"jobs": model.job_tree()})
+
+    def post(self):
+        return self.json({"TODO": "make this start a job of some sort"})
+
+
+class JobHandler(JSONHandler):
+    def get(self, job_id):
+        return self.json({"TODO": "get info for given job"})
+
+    def post(self, job_id):
+        return self.json({"TODO": "update a job"})
+
 
 ROUTES = [
     (r"/", UIHandler),
@@ -212,26 +268,27 @@ ROUTES = [
     (r"/v0/text/chat", TranscribeHandler),
     (r"/v0/text/generate", TextCompletionHandler),
     (r"/v0/image/describe", DescribeImageHandler),
-    (r"/v0/image/from_prompt", ImageHandler)
+    (r"/v0/image/from_prompt", ImageHandler),
+    (r"/v1/job", JobsHandler),
+    (r"/v1/job/([0-9]+)", JobHandler),
 ]
 
-THREAD = None
 
 async def main(port):
-    global THREAD
     print("Setting up app...")
-    static_path = os.path.join(os.path.dirname(__file__), 'static/')
+    static_path = os.path.join(os.path.dirname(__file__), "static/")
     print(f"  static serving {static_path} ...")
     app = tornado.web.Application(
         ROUTES,
         debug=True,
         default_handler_class=TrapCard,
         static_path=static_path,
-        static_url_prefix= "/static/"
+        static_url_prefix="/static/",
     )
     print(f"  listening on {port}...")
     app.listen(port)
     await asyncio.Event().wait()
+
 
 if __name__ == "__main__":
     asyncio.run(main(8080))
